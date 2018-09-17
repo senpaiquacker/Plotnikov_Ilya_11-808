d09fc092d088c74e6e89e6135533d2dbafa16a73
PlotnikovIlya <ilaya.tanks@gmail.com>
2018-09-17 08:01:22 +0300

    last sem

---
+++ 2018/fall/SEM/sem 1.7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //entering k and m
+            double k = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double m = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            //adding variables for position of both vectors and instantly finding it
+            //because we don't need an exact vector, we can set potential x1 = 0, potential x2 = 1, and y1 = k * x1 + m, y2 = k * x2 + m then
+            //so coordinates of our vector will be (1 - 0; k * 1 + m - (k * 0 + m)) => (1; k)
+            // but this is only for parallel vector. For perpendicular our y1 = (-1) * (1/k) * 0 + m, y2 = (-1) * (1/k) * 1 + m
+            //and these coordinates will be (1; -1/k);
+            double vec1x = 1;
+            double vec1y = k;
+            double vec2x = 1;
+            double vec2y = (-1) * (1 / k);
+            //output
+            Console.Write(vec1x);
+            Console.Write(" ");
+            Console.WriteLine(vec1y);
+            Console.Write(vec2x);
+            Console.Write(" ");
+            Console.WriteLine(vec2y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2018/fall/SEM/sem 1.8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inputting position of dots included in line
+            double k = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double m = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //inputting dot position
+            double px = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double py = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //finding our m in y = -(1/k)*x+m
+            double m2 = py + (1 / k) * px;
+            //finding x pos:
+            double v = (k - (1 / k));
+            double x2 = (m2 - m) / v;
+            //finding y pos:
+            double y2 = k * x2 + m;
+            //output
+            Console.Write(x2);
+            Console.Write(" ");
+            Console.Write(y2);
+            Console.ReadKey();
+        }
+    }
+}
