1f7f2822349c759e140f2d141463ab5fe513d088
PlotnikovIlya <ilaya.tanks@gmail.com>
2018-09-23 17:49:50 +0300

    Pluralize Practice

---
+++ 2018/fall/PR/PluralizeTask.cs
@@ -0,0 +1,23 @@
+﻿namespace Pluralize
+{
+    public static class PluralizeTask
+    {
+        public static string PluralizeRubles(int count)
+        {
+            string argum;
+            if (((count % 100 > 10) && (count % 100 < 20)) || (count % 10 > 4) || count % 10 == 0)
+            {
+                argum = "лей";
+            }
+            else if(count % 10 != 1)
+            {
+                argum = "ля";
+            }
+            else
+            {
+                argum = "ль";
+            }
+			return "руб"+argum;
+		}
+	}
+}
