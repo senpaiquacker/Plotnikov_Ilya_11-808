d1bab743ddb9af827c5c7cf398e98a647f36ff91
Plotnikov Ilya <ilaya.tanks@gmail.com>
2019-03-26 13:20:32 +0300

    Linq Exercises

---
+++ 2019/spring/MISC/Linq Exercises/FirstTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var stringArray = new string[n];
+            for(int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+            int k = 1;
+            var answer = stringArray
+                .Where(z => z != string.Empty)
+                .Select(z => z + k++);
+            foreach(var i in answer)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2019/spring/MISC/Linq Exercises/SecondTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    static class IEnumerableExtensions
+    {
+        public static IEnumerable<TItem> MakeStrangeThing<TItem>
+            (this IEnumerable<TItem> ienumerable, Func<int, IEnumerable<TItem>> function)
+        {
+            int i = 0;
+            var result = new List<TItem>();
+            foreach(var item in ienumerable)
+            {
+                result.Add(item);
+                if(i % 2 !=0)
+                {
+                    result.AddRange(function(i));
+                }
+            }
+            return result as IEnumerable<TItem>;
+        }
+    }
+}
